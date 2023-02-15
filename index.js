124dd7657ba78681cedc3549924a388f13132428
subhadipmanna70 <subhadipmanna70@gmail.com>
2023-02-15 12:25:37 +0530

    setHeader fro cross origin implemented

--- index.js
+++ index.js
@@ -12,7 +12,10 @@ const jwtKey=process.env.jwt_Key;
 
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Headers", "content-type");
+    res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS");
     next();
   });
 
